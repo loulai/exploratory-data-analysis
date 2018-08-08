3e0054351c7344d0709dee68d49e63f96c7a343e
Louise Lai <louiselai88@gmail.com>
2018-08-07 22:19:30 -0700

    going swell

--- hw3/garbage.R
+++ hw3/garbage.R
@@ -55,3 +55,162 @@ hackydf %>%
         axis.text.y=element_blank(),
         axis.ticks.y=element_blank()) +
   labs(title="Officers (by race), most likely to stop drivers of race:")
+
+
+########################
+```{r}
+# Given that some legal action resulted, how many of those arrested are of each ethnicity?
+
+# first, let's explore. How many legal actions were taken?
+totalStops <- nrow(df) # 79,884
+totalLegalActions <- df %>% filter(Result_of_Stop == "Citation Issued" | Result_of_Stop == "Arrest") %>% nrow() # 34,506
+totalLegalActions/totalStops # 43% of stops result in a legal action
+
+legalActiondf <- df %>% filter(Result_of_Stop == "Citation Issued" | Result_of_Stop == "Arrest")
+
+df2 <- legalActiondf %>% 
+  group_by(Driver_Race) %>% 
+  summarize(ethnicityArrestedPercent = 100 * n()/totalLegalActions) %>% 
+  arrange(desc(ethnicityArrestedPercent)) # black = 53%, White 42%
+
+pie <- df2 %>% 
+  mutate(ethnicityArrestedPercent = ethnicityArrestedPercent) %>% 
+  ggplot(aes(x="", y=ethnicityArrestedPercent, fill=Driver_Race)) +
+  geom_bar(width=1, stat="identity") + 
+  coord_polar("y", start=0) +
+  labs(title="Given arrested, what is their ethnicity?", xlab="", ylab="") +
+  geom_text(aes(y = ethnicityArrestedPercent/5 + c(0, cumsum(ethnicityArrestedPercent)[-length(ethnicityArrestedPercent)]), 
+                label = percent(ethnicityArrestedPercent/100)), size=3)+ 
+  scale_fill_brewer()
+
+pie
+
+# Wow. Now, let's say you've been stopped. Given that you're X race, 
+# are you more or less likely to be arrested?
+
+# create a legal action column and select only relevant columns
+legalActiondf <- df %>% 
+  mutate(legal_action_taken = Result_of_Stop == "Citation Issued" | Result_of_Stop == "Arrest") %>% 
+  select(-1, -5, -7)
+
+likelihood_to_be_arrested_given_race <- legalActiondf %>% 
+  group_by(Driver_Race) %>% 
+  summarize(percent_legal_action_taken = 100 * sum(legalAction == T)/n()) %>% 
+  ungroup() %>% 
+  arrange(desc(percent_legal_action_taken))
+
+likelihood_to_be_arrested_given_race
+pie2 <- df2 %>% 
+  mutate(ethnicityArrestedPercent = ethnicityArrestedPercent) %>% 
+  group_by(ethnicityArrestedPercent) 
+
+pie2
+
+# Given ethnicity, how likely are you to be stopped? Actually can't answer this! Coz we only see people who have already been stopped
+
+###################################################################
+# Data Exploration
+glimpse(df) # 79884 observations, 12 variables
+
+# How many officers, by race
+df %>% 
+  count(Officer_Race) %>% 
+  arrange(desc(n)) 
+
+df %>% 
+  mutate(Officer_Race = Officer_Race %>% fct_infreq()) %>% 
+  ggplot(aes(Officer_Race)) +
+  geom_bar() +
+  xlab("Race of the Officer") +
+  ylab("Number of Officers") +
+  labs(title="Count of Officers by Race") +
+  scale_y_continuous(breaks=seq(0, 60000, 10000), labels=comma_format()) +
+  theme_classic() +
+  theme(
+    axis.text.x=element_text(angle=60, hjust=1),
+    panel.background = element_rect(fill="lightblue"),
+    panel.grid.minor = element_line(size=0.25, linetype='solid',
+                                    color='white'),
+    text=element_text(family="Helvetica"))
+
+# How many drivers stopped, by race
+df %>% 
+  count(Driver_Race) %>% 
+  arrange(desc(n))
+
+df %>% 
+  mutate(Driver_Race = Driver_Race %>% fct_infreq()) %>% 
+  ggplot(aes(Driver_Race)) +
+  geom_bar() +
+  xlab("Race of the Driver") +
+  ylab("Number of Drivers") +
+  labs(title="Count of Drivers by Race") +
+  scale_y_continuous(breaks=seq(0, 50000, 10000), labels=comma_format()) +
+  theme_classic() +
+  theme(
+    axis.text.x=element_text(angle=60, hjust=1),
+    panel.background = element_rect(fill="lightblue"),
+    panel.grid.minor = element_line(size=0.25, linetype='solid',
+                                    color='white'),
+    text=element_text(family="Helvetica"))
+
+# we see that it's mostly white officers, by a large amount. Come back and see percentages.
+```
+
+```{r}
+# Do officers have a biases towards stopping certain races?
+
+# how many stops per officer race?
+dfStoppedRace <-  df %>% 
+  group_by(Officer_Race) %>% 
+  mutate(driver_stops_made = sum(n())) %>% 
+  ungroup()
+
+# get a count of how many stops made, by ethnicity
+dfStoppedRaceSummarized <-  df %>% 
+  group_by(Officer_Race) %>% 
+  summarize(driver_stops_made = sum(n())) %>% 
+  arrange(desc(driver_stops_made)) %>% 
+  top_n(5)
+
+
+dfStoppedRace %>% 
+  group_by(Officer_Race, Driver_Race) %>% 
+  summarize(percent_race_stopped = 100 * n()/driver_stops_made[1]) %>% 
+  ungroup() %>%
+  group_by(Officer_Race) %>% 
+  arrange(desc(percent_race_stopped), .by_group=T) 
+
+dfStoppedRace$driver_stops_made[2]
+
+# What percent of stops of people actually lead to an arrest?
+# 42
+
+# Driver race ~ officer race <<<<<<<<< good! Could add count but meh
+dfStoppedRace %>% 
+  mutate(Officer_Race = Officer_Race %>% fct_infreq()) %>% 
+  ggplot(aes(Officer_Race)) +
+  geom_bar(aes(fill=Driver_Race)) + 
+  scale_fill_brewer(palette="Green", name="Driver Race") +
+  theme(axis.text.x=element_text(angle=10, hjust=1)) +
+  xlab("Officer Race") + 
+  ylab("Count of Drivers Stopped")
+
+#############################################################################################
+# What about initial Reason for Stop?
+likelyAlluvia2 <- df %>% 
+  mutate(legal_action_taken = Result_of_Stop == "Citation Issued" | Result_of_Stop == "Arrest") %>% 
+  group_by(legal_action_taken, Reason_for_Stop, Driver_Gender) %>% 
+  summarise(Frequency=n())
+
+is_alluvia_form(likelyAlluvia2, axes=1:3, silent=TRUE)
+
+likelyAlluvia2 %>% 
+  ggplot(aes(y = Frequency, axis2 = Driver_Gender, axis1=Reason_for_Stop)) +
+  geom_alluvium(aes(fill = legal_action_taken), width = 1/12) +
+  geom_stratum(width = 1/12, fill = "black", color = "grey") +
+  geom_label(stat = "stratum", label.strata = TRUE) +
+  scale_x_discrete(limits = c("Gender", "Reason for Stop"), expand = c(.1, .1)) +
+  scale_fill_brewer(type = "qual", palette = "Set1", name="Legal Action Taken") +
+  ggtitle("Was Legal Action Taken? By Initial Reason for Stop and Gender")
+
