70763e7fc618a423cb64cc1a8dc6bf5a55340a29
Louise Lai <louiselai88@gmail.com>
2018-08-13 07:24:00 -0700

    generic case handled

--- hw4/kmeans.R
+++ hw4/kmeans.R
@@ -11,11 +11,11 @@ myScatterInput <- as.data.frame(matrix(runif(12, 1, 10), ncol=m))
 
 points <- myScatterInput
 n <- nrow(myScatterInput)
-myClusterNum <- as.integer(runif(1, 2, n))
+myClusterNum <- 2 #as.integer(runif(1, 2, n))
 
 # 1) random assignment
 points <- points %>% 
-  mutate(clusterAssignment = as.integer(runif(n, 1, myClusterNum)))
+  mutate(clusterAssignment = rep_len(1:myClusterNum, n))
 
 # 2) compute centroids
 centriods <- c()
@@ -24,18 +24,15 @@ for(i in 1:myClusterNum){
                        filter(clusterAssignment == i) %>% 
                        colMeans())
 }
-centriods <- na.omit(centriods) # is this hella inefficient?! Come back
 
 # 3) distance from each data point to centriod 
 # by rbinding the centriods to dataframe of points, the dist() function can be used
-pointsCentriods <- rbind(points, centriods) %>% select(-clusterAssignment)
 distanceMatrix <- as.data.frame(as.matrix(dist(pointsCentriods)))
 
 # 4) assign each point to the cluster centriod with minimal distance
 distanceMatrix
-totalClusters <- nrow(distanceMatrix) - n
 for(i in 1:n){
-  for(k in 1:totalClusters){
+  for(k in 1:myClusterNum){
     
   }
 }
